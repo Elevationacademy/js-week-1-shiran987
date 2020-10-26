e5b8ecdc8bd909bf345bf07d383da36ffbf48359
shiran987 <73052834+shiran987@users.noreply.github.com>
2020-10-26 14:26:08 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+// ex 1
+const maxCapacity = 14
+let surfTime = true
+let bestStudent
+//const purposeInLife 
+console.log( maxCapacity)
+console.log(surfTime)
+console.log(bestStudent)
+//console.log( purposeInLife )
+
+//ex2 
+const myVariable = "Let's try this "
+	
+const result = myVariable + "is the best thing ever"
+console.log(result)
+
+//ex3
+const password = "shiranshakra"
+const confirmPassword = "shiranshakra"
+
+console.log(password == confirmPassword)
+
+//ex4
+const x =423 * 12
+const y =802 / 2
+const z =(5 + 6) * 3
+console.log(x)
+console.log(y)
+console.log(z)
